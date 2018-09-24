b35a95b66ea3cd5895ce676ae50287074fd3738a
An Trinh <an.trinhht@gmail.com>
2018-09-24 09:17:36 +0700

    leanpool

---
+++ GitHub Rainy Day/Assets/Resources/Scripts/Pool/AudioSource_.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lean.Pool
+{
+	public class AudioSource_ : MonoBehaviour 
+	{
+		
+		public float count=0;
+		public AudioSource audioSource ;
+
+		private bool isOnEnable;
+
+		private void OnEnable ()
+		{
+			
+			isOnEnable = true;
+		}
+		private void OnDisable ()
+		{
+			isOnEnable = false;
+		}
+
+		public void Play ()
+		{
+			audioSource.Play ();
+		}
+		// Update is called once per frame
+		void Update () {
+			if (!isOnEnable) {
+				return;
+			}
+
+			LeanPoolTestOF.instance.DespawnPrefab (LeanPoolTestOF.instance.spawnedPawnPrefabs,audioSource.clip.length);
+		}
+	}
+}
