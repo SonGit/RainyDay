03fd36e9c18925c787276a4bba540cf91756cba4
Son <sonpd@avr-solutions.com>
2018-10-09 13:29:26 +0700

    Fix spline bug

--- GitHub Rainy Day/Assets/Resources/Scripts/RainGirl/GRDrawGPSLine.cs
+++ GitHub Rainy Day/Assets/Resources/Scripts/RainGirl/GRDrawGPSLine.cs
@@ -4,43 +4,43 @@ using UnityEngine;
 using geniikw.DataRenderer2D;
 
 public class GRDrawGPSLine : MonoBehaviour {
-
+	[SerializeField]
 	WorldLine worldLine;
-
-	Transform origin;
-
-	bool drawing;
 
 	// Use this for initialization
 	void Start () {
 		worldLine = this.GetComponent<WorldLine> ();
+		worldLine.MakeNewMesh ();
 	}
 
 	public void DrawPath(List<Vector3> nodes)
 	{
 		worldLine = this.GetComponent<WorldLine> ();
-		this.origin = origin;
-		int i = 0;
-
+	
 		worldLine.line.Clear ();
-		worldLine.MakeNewMesh ();
 
 		foreach (Vector3 node in nodes) {
-
-			worldLine.line.Push ();
-			worldLine.line.EditPoint (i,node + new Vector3(0,.025f,0),.05f);
-			i++;
+			worldLine.line.Push (node + new Vector3(0,.025f,0),Vector3.zero,Vector3.zero,.05f);
 		}
-
-
-		drawing = true;
 	}
 
 	void Update()
 	{
-		if (drawing) {
-			//worldLine.line.EditPoint (0,origin.position,.1f);
-		}
+
 	}
+
+	public void PopANode(List<Vector3> nodes)
+	{
+		for (int i = 0; i < nodes.Count; i++) {
+			// Fix for the spline's bug
+			if (i == 1) {
+				worldLine.line.EditPoint( i, new Point (nodes[i],new Vector3(0,-.01f,0),new Vector3(0,0,0),0.05f));
+			} else {
+				worldLine.line.EditPoint( i, new Point (nodes[i],Vector3.zero,Vector3.zero,0.05f));
+			}
+		}
+			
+	}
+
 
 }
