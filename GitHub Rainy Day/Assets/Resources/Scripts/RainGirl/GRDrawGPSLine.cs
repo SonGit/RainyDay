c26dd775d6ebcd265e9b754217a37a506e14a4fa
Son <sonpd@avr-solutions.com>
2018-10-10 18:11:46 +0700

    GPS line

--- GitHub Rainy Day/Assets/Resources/Scripts/RainGirl/GRDrawGPSLine.cs
+++ GitHub Rainy Day/Assets/Resources/Scripts/RainGirl/GRDrawGPSLine.cs
@@ -5,41 +5,31 @@ using geniikw.DataRenderer2D;
 
 public class GRDrawGPSLine : MonoBehaviour {
 	[SerializeField]
-	WorldLine worldLine;
+	LineRenderer lineRenderer;
+	[SerializeField]
+	LineRenderer lineRendererToAI;
 
 	// Use this for initialization
 	void Start () {
-		worldLine = this.GetComponent<WorldLine> ();
-		worldLine.MakeNewMesh ();
+		
 	}
 
-	public void DrawPath(List<Vector3> nodes,Transform org)
+	public void DrawPath(Vector3[] nodes, Vector3 AIpos)
 	{
-		worldLine = this.GetComponent<WorldLine> ();
+		lineRenderer.SetPositions(nodes);
 
-		this.org = org;
-	
-		worldLine.line.Clear ();
-
-		foreach (Vector3 node in nodes) {
-			worldLine.line.Push (node,Vector3.zero,Vector3.zero,.05f);
+		for (int i = 1; i < nodes.Length; i++) {
+			lineRenderer.SetPosition (i,nodes[i]);
 		}
+
+		lineRendererToAI.SetPosition (0,lineRenderer.GetPosition(0));
+		lineRendererToAI.SetPosition (1,AIpos);
 	}
 
 	void Update()
 	{
-		if (first && org != null) {
-			worldLine.line.EditPoint (0,new Vector3((float)System.Math.Round(org.position.x,1),(float)System.Math.Round(org.position.y,1),(float)System.Math.Round(org.position.z,1)),0.05f);
-		}
+		
 	}
+		
 
-	bool first;
-	Transform org;
-
-	public void PopANode(List<Vector3> nodes)
-	{
-		if(first)
-		worldLine.line.PopFirst ();	
-		first = true;
-	}
 }
